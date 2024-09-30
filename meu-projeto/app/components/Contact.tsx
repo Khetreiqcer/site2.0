70b28981adf76795ad389314513ecb5a71fb0bc4
Khetreiqcer <jvpossenti666@gmail.com>
2024-09-30 14:26:28 -0300

    Initial commit for backend

--- meu-projeto/app/components/Contact.tsx
+++ meu-projeto/app/components/Contact.tsx
@@ -13,7 +13,7 @@ export default function ContactForm() {
     e.preventDefault(); // Evitar o comportamento padrão do formulário
 
     try {
-      const response = await fetch('http://localhost:5000/api/contact', {
+      const response = await fetch('https://sitev2-8e04632047ba.herokuapp.com/api/contact', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
